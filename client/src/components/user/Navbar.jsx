6434a8b276a6a6eefdda9c6e9bb13e723874cff0
Baibhav Sinha <baibhavsinha.dev@gmail.com>
2026-06-29 17:04:17 +0530

    feat: added clerk authentication in project and used it in navbar

--- client/src/components/user/Navbar.jsx
+++ client/src/components/user/Navbar.jsx
@@ -1,18 +1,39 @@
 import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
+import { useClerk, UserButton } from "@clerk/react";
+import { useAppContext } from "../../context/AppContext";
 
 const Navbar = () => {
+
+    const { openSignIn } = useClerk();
+    const { user } = useAppContext();
+
     return (
         <div className="shadow py-4">
             <div className="px-4 xl:px-20 mx-auto flex justify-between items-center">
                 <Link to="/">
-                    <img src={assets.logo} alt="logo"  />
+                    <img src={assets.logo} alt="logo" />
                 </Link>
 
-                <div className="flex gap-4 max-sm:text-xs">
-                    <button className="text-gray-600">Recruiter Login</button>
-                    <button className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">Login</button>
-                </div>
+
+                {user ? (
+                    <div className="flex items-center gap-3">
+                        <Link to="/applications">Applied Jobs</Link>
+
+                        <p>|</p>
+                        <p>Hi, {(user.firstName + " " + user.lastName) || "User"}</p>
+                        
+                        <UserButton />
+                    </div>
+                ) : (
+                    <div className="flex gap-4 max-sm:text-xs">
+                        <button className="text-gray-600">Recruiter Login</button>
+
+                        <button onClick={openSignIn} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">
+                            Login
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,22 @@
+import { ClerkProvider } from '@clerk/react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx';
 
+// Import Clerk PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+    throw new Error("PUBLISHABLE is not provided");
+};
+
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <AppProvider>
-            <App />
-        </AppProvider>
-    </BrowserRouter>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <BrowserRouter>
+            <AppProvider>
+                <App />
+            </AppProvider>
+        </BrowserRouter>
+    </ClerkProvider >
 );
